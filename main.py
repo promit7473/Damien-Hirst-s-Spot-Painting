ed95a3894bc02282bdd26ddd8398acd7ac756dd1
Meraj Hossain Promit <merajhossainpromit@gmail.com>
2024-04-23 10:04:51 +0600

    deploy

---
+++ main.py
@@ -0,0 +1,47 @@
+# import colorgram
+#
+# colors = colorgram.extract('spopt_painting.jpg', 30)
+# rgb_colors = []
+#
+#
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+#
+# print(rgb_colors)
+import turtle as turtle_module
+import random
+
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+color_list = [(224, 233, 227), (207, 160, 82), (54, 88, 130), (145, 91, 40), (140, 26, 49), (221, 207, 105), (132, 177, 203), (158, 46, 83), (45, 55, 104), (169, 160, 39), (129, 189, 143), (83, 20, 44), (37, 43, 67), (186, 94, 107), (187, 140, 170), (85, 120, 180), (59, 39, 31), (88, 157, 92), (78, 153, 165), (194, 79, 73), (45, 74, 78), (80, 74, 44), (161, 201, 218), (57, 125, 121), (219, 175, 187), (169, 206, 172), (219, 182, 169)]
+tim.setheading(220)
+tim.forward(350)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots+1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+# tim.setheading(90)
+# tim.forward(50)
+# tim.setheading(180)
+# tim.forward(500)
+
+screen = turtle_module.Screen()
+screen.exitonclick()
